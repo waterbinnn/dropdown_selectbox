58012df47a95f269ec6eee6c31a3c87963d8e102
Subin <ewaterbin@naver.com>
2022-04-16 18:34:10 +0900

    js ing

---
+++ script.js
@@ -0,0 +1,16 @@
+// 자바스크립트로 버튼토글 구현하기 
+
+const btn = document.querySelector(".toggle-btn");
+const list = document.querySelector(".selectbox-option");
+
+btn.addEventListener('click',() => {
+  list.classList.toggle('on');
+  btn.classList.toggle('on');
+});
+
+list.addEventListener('click',(event)=>{
+  (event.target.nodeName === "BUTTON") {
+    btn.innerText = event.target.innerText;
+    list.classList.remove('on');
+}
+});
