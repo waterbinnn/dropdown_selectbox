91d0b5bc54f72c413e2b277024db49c70fa2ccb1
Subin <ewaterbin@naver.com>
2022-04-16 20:05:11 +0900

    js first commit

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ btn.addEventListener('click',() => {
 });
 
 list.addEventListener('click',(event)=>{
-  (event.target.nodeName === "BUTTON") {
+  (event.target.nodeName === "BUTTON"); {
     btn.innerText = event.target.innerText;
     list.classList.remove('on');
 }
